0bb39f9df04d2ecb5f257d0ef88984ba6364e97f
AnirudhArrepu <anirudharrepu@gmail.com>
2025-04-16 16:11:52 +0530

    file fixes

---
+++ cobal-test/testing/test1.cbl
@@ -0,0 +1,23 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. BankOverflow.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 TOTAL-AMOUNT     PIC 9(5) VALUE ZEROS.
+01 TRANSACTION-AMT  PIC 9(5).
+
+PROCEDURE DIVISION.
+    DISPLAY "Enter transaction amount: " WITH NO ADVANCING.
+    ACCEPT TRANSACTION-AMT.
+    
+    ADD TRANSACTION-AMT TO TOTAL-AMOUNT.
+    
+    IF TOTAL-AMOUNT > 99999
+        DISPLAY "ERROR: Overflow occurred in total amount calculation."
+    ELSE
+        DISPLAY "Updated Total Amount: " TOTAL-AMOUNT.
+    END-IF.
+
+    STOP RUN.
+
+ 
